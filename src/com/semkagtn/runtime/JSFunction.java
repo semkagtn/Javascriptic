8fa2dc2bc83952be871ef3076a650b9683b530fa
semkagtn <semkagtn@gmail.com>
2013-11-29 22:52:52 +0400

    Few fixes

--- src/com/semkagtn/runtime/JSFunction.java
+++ src/com/semkagtn/runtime/JSFunction.java
@@ -2,6 +2,27 @@ package com.semkagtn.runtime;
 
 public abstract class JSFunction extends JSString {
 	
+	private static final String DEFAULT_TEXT = "[native code]";
+	
+	public static final JSFunction PRINT = new JSFunction(DEFAULT_TEXT) {
+		public JSObject call(JSObject... objects) {
+			if (objects.length == 0) {
+				System.out.print(JSUndef.UNDEF);
+			} else if (objects[0] instanceof JSNumber) {
+				String number = objects[0].value;
+				int t = number.length() - 2;
+				if (number.substring(t).equals(".0")) {
+					System.out.print(number.substring(0, t));
+				} else {
+					System.out.print(number);
+				}
+			} else {
+				System.out.print(objects[0]);
+			}
+			return JSUndef.UNDEF;
+		}
+	};
+	
 	public JSFunction(String text) {
 		super(text);
 	}
--- test.js
+++ test.js
@@ -1,20 +1,72 @@
 /*
- * Вызов функций.
- * Аргументы функции.
+ *
+ * Аргументы функции. (не полные)
  * Добавить стандартные функции
+ * Допилить аргументы.
+ * Разобраться со стеком.
+ *
  */
 var print;
-var Counter = function() {
-    var i = 0;
-    return function() {
-        print((i = i + 1) + "\n");
-    };
-};
 
-var c1 = Counter();
-var c2 = Counter();
-c1();
-c1();
-c1();
-c2();
-c1();
+// 1. 1 2 3 4 5 6 7 8 9
+print("1. while test: ");
+var i = 0;
+while ((i = i + 1) < 10) {
+    print(i + " ");
+}
+print("\n\n");
+
+// 2. String comparing
+if ("abc" < "abd") {
+    print("2. 'abc' < 'abd'\n\n");
+}
+
+// 3. NaN
+print("3. 'true' * 500 == ");
+print("true" * 500);
+print("\n\n");
+
+// 4. undefined
+var undef;
+print("4. " + undef + "\n\n");
+
+// 5. Print function, but without whitespaces :(
+var f = function() {
+    return undefined;
+};
+print("5. " + f + "\n\n");
+
+// 6. Implicit type conversion
+if ("123" == 123)
+    print("6. Yes, '123' == 123\t O_O\n\n");
+
+// 7. Oh my JavaScript!
+print("7.\n");
+f = function() {
+    print("First ");
+    f = function() {
+        print("WTF? O_O\n");
+    };
+    print("call\n");
+};
+f();
+f();
+print("\n");
+
+// 8. Funny cast number to bool (double not)
+print("8. " + !!0 + " " + !!1 + "\n\n"); // false true
+
+// 9. Typical JavaScript code
+var func = function() {
+    print(outer_var);
+};
+var outer_var = "9. :D";
+func();
+print("\n\n");
+
+
+
+
+
+
+
