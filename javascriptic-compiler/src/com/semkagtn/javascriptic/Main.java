8138bff8f77fb165c40e96cd646a48ab37e7e982
semkagtn <semkagtn@gmail.com>
2013-11-30 05:38:52 +0400

    Revert "deleted some files"
    This reverts commit a10da2ad3af9727e93949b2e51e04144bf960587.

--- javascriptic-compiler/src/com/semkagtn/javascriptic/Main.java
+++ javascriptic-compiler/src/com/semkagtn/javascriptic/Main.java
@@ -4,11 +4,11 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Compiler compiler = new Compiler();
-		if (args.length == 0) {
+		/*if (args.length == 0) {
 			System.out.println("Usage: source (target)?");
 			System.exit(1);
-		}
-		compiler.setInputFileName(args[0]);
+		}*/
+		compiler.setInputFileName("test.js");
 		String target = args.length > 1 ? args[1] : "Out";
 		compiler.setOutputFileName(target);
 		compiler.run();
