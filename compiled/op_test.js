2b5ea07afc9cd82d044982f9a4017e8a45b4e844
semkagtn <semkagtn@gmail.com>
2013-12-02 18:15:08 +0400

    New runtime library

---
+++ compiled/op_test.js
@@ -0,0 +1,13 @@
+print(("NaN" == NaN) + "\n");       // false
+print((NaN == NaN) + "\n");         // false
+print((true == 1) + "\n");          // true
+print((true == 42) + "\n");         // false
+print(false + "\n");
+print((0 == "") + "\n");            // true
+print(("" == 0) + "\n");            // true
+print(("false" == false) + "\n");   // false
+print((false == 0) + "\n");         // true
+print((undefined == false) + "\n"); // false
+print(false + "\n");
+print(true + "\n");
+print((" \t\r\n " == 0) + "\n");    // truek
